70d7207d6965a81b28e48a75b399c12b23486572
kartiknawal <kartiknawal.7@gmail.com>
2020-09-29 19:08:08 +0530

    [kartiknawal] add check equality of lines

--- Program.cs
+++ Program.cs
@@ -7,15 +7,24 @@ namespace LineComparisionProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
-            Console.WriteLine("Length of the line P1P2 is : " + lengthOfLine());
+            Console.WriteLine("Line 1");
+            double lengthOfLine1 = lengthOfLine();
+            Console.WriteLine("Length of line 1: " + lengthOfLine1);
+            Console.WriteLine("Line 2");
+            double lengthOfLine2 = lengthOfLine();
+            Console.WriteLine("Length of line 2: " + lengthOfLine2);
+            if (lengthOfLine1 == lengthOfLine2)
+                Console.WriteLine("Both lines are equal");
+            else
+                Console.WriteLine("Both lines are unequal");
         }
         static double lengthOfLine()
         {
-            Console.WriteLine("Enter the X & Y coordinates of P1");
+            Console.WriteLine("Enter the X & Y coordinates of P1 (starting point)");
             double X1 = double.Parse(Console.ReadLine());
             double Y1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the X & Y coordinates of P2");
+            Console.WriteLine("Enter the X & Y coordinates of P2 (ending point)");
             double X2 = double.Parse(Console.ReadLine());
             double Y2 = double.Parse(Console.ReadLine());
 
