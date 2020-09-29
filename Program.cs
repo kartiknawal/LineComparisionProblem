117d3a793c2cb4a2c9433a06b2d2f929f144b153
kartiknawal <kartiknawal.7@gmail.com>
2020-09-29 16:50:38 +0530

    [kartiknawal] add initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisionProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
